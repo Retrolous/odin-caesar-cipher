b2be7617f5e032ce9162a589d77541e412ac3843
retrolous <menomelas@gmail.com>
2024-08-31 22:30:25 +0100

    Change variables names from camelCase to snake_case
    Still used to JavaScript

--- script.rb
+++ script.rb
@@ -1,12 +1,12 @@
 require "pry-byebug"
 
 def caeser_cipher(string, shift = 0) 
-  actualShift = shift.to_i % 26 # find out the actual shift in value, ignoring any loops (increments / decrements greater than 26)
-  codepointsArray = string.to_s.codepoints # convert each character to a codepoint for modification
-  shiftedArray = codepointsArray.map do |codepoint|
+  actual_shift = shift.to_i % 26 # find out the actual shift in value, ignoring any loops (increments / decrements greater than 26)
+  codepoints_array = string.to_s.codepoints # convert each character to a codepoint for modification
+  shifted_array = codepoints_array.map do |codepoint|
     if codepoint < 65 || codepoint > 122 then next codepoint.chr(Encoding::UTF_8) end # although there is no fundamental issue in doing so, do not modify the character if not in the latin alphabet
 
-    modified = codepoint + actualShift
+    modified = codepoint + actual_shift
 
     case 
       when modified < 65 then modified += 25 # if the modified codepoint ends up being less than the codepoint for "A", loop forward
@@ -17,7 +17,7 @@ def caeser_cipher(string, shift = 0)
     
     modified.chr(Encoding::UTF_8)
   end
-  shiftedArray.join("")
+  shifted_array.join("")
 end
 
 # puts caeser_cipher("aTtAckatdaWN", 2) # works normally
